f21cd435aa7a3acbba5dbe8e03f32a00a26fae5b
its-coder5128 <avnish5128uniyal@gmail.com>
2023-12-30 21:53:56 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <div className=' font-extrabold text-white text-2xl bg-black'>
+      hello
+    </div>
+  )
+}
+
+export default App
